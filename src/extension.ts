b7d50583b4d84e7dc4575bc6fa452b932ee7be8a
nazarpunk <nazarpunk@gmail.com>
2023-07-28 22:28:29 +0300

    translate

--- src/extension.ts
+++ src/extension.ts
@@ -7,15 +7,11 @@ import ExtSemanticTokensLegend from "./semantic/ext-semantic-tokens-legend";
 import JassParser from "./jass/jass-parser";
 import JassTokensList from "./jass/jass-tokens-list";
 import WtsTokensList from "./wts/wts-tokens-list";
-import i18next from "i18next";
-import {i18n} from "./utils/i18n";
 
 // noinspection JSUnusedGlobalSymbols
 export function activate(context: ExtensionContext) {
     const jass = new ExtProvider('jass', JassTokensList, JassParser, JassVisitor);
     const wts = new ExtProvider('wts', WtsTokensList, WtsParser, WtsVisitor);
-
-    console.log(i18next.t(i18n.stringIndexRedeclareError));
 
     context.subscriptions.push(
         languages.registerDocumentSemanticTokensProvider({language: jass.name}, jass, ExtSemanticTokensLegend),
